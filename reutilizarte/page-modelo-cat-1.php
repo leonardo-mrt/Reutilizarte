833fa31aee54a32179ae99f84826b7b27f00233f
Leonardo Martins <sleonardo.mrt@gmail.com>
2021-04-19 17:01:38 -0300

    Modelo de página (1) de [categoria]
    Categorias:
    Garrafa PET
    Embalagens
    Papelão
    Plástico
    Outros

---
+++ reutilizarte/page-modelo-cat-1.php
@@ -0,0 +1,150 @@
+<?php
+// Template Name: Modelo Categoria (pg1)
+?>
+
+<?php get_header(); ?> 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <!-- INÍCIO - INTRODUÇÃO -->
+    <section class="introducao-interna interna-cat-embalagens">
+      <div class="container">
+        <h1 class="grid-16">embalagens</h1>
+      </div>
+    </section>
+    <!-- FIM - INTRODUÇÃO -->
+
+    <!-- INÍCIO - DESTAQUES -->
+    <section class="container destaques">
+
+        <div class="grid-8 destaques-esq">
+          <div>
+          <a href=""><img src="./img/categoria-1/cat-destaque-1.jpg" alt="destaque-1"></a>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed [...]</p>
+          </div>
+        </div>
+
+        <div class="grid-8 destaques-dir">
+          <div>
+            <div>
+              <a href=""><img src="./img/categoria-1/cat-destaque-2.jpg" alt="destaque-2"></a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed [...]</p>
+            </div>
+            <div>
+              <a href=""><img class="destaque-3" src="./img/categoria-1/cat-destaque-3.jpg" alt="destaque-3"></a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed [...]</p>
+            </div>
+          </div>
+        </div>
+    </section>
+    <!-- FIM - DESTAQUES -->
+    
+    <!-- INÍCIO - PRINCIPAIS ARTIGOS -->
+    <section class="artigos">
+
+      <div class="container">
+        <h2 class="grid-16 sub-interno-cat">Artigos</h2>
+      </div> 
+      
+      <!-- INÍCIO - ARTIGO 1 -->
+      <div class="container mod-artigo">
+        <div class="img-artigo">
+        <a href=""><img src="img/categoria-1/cat-img-1.jpg" alt="Artigo 1" class="grid-5"></a>
+        </div>
+
+        <div class="text-artigo grid-11">
+            <h3 class="categoria-artigo">CATEGORIA</h3>
+            <a href="">
+              <h2 class="titulo-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor </h2>
+            </a>
+            <p class="autor-artigo">Publicado por [NOME] em [MES/ANO/]</p>
+            <p class="previa-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien eget mi proin sed. uis imperdiet massa tincidunt nunc pulvinar. Orci eu lobortis [...]</p>
+            <a href="" class="btn-leia_mais">leia mais</a>
+        </div>
+      </div>
+      <!-- FIM - ARTIGO 1 -->
+
+      <!-- INICIO - ARTIGO 2 -->
+      <div class="container mod-artigo">
+        <div class="img-artigo">
+          <a href=""><img src="img/categoria-1/cat-img-2.jpg" alt="Artigo 2" class="grid-5"></a>
+        </div>
+
+        <div class="text-artigo grid-11">
+          <h3 class="categoria-artigo">CATEGORIA</h3>
+          <a href="">
+            <h2 class="titulo-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor </h2>
+          </a>
+          <p class="autor-artigo">Publicado por [NOME] em [MES/ANO/]</p>
+          <p class="previa-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien eget mi proin sed. uis imperdiet massa tincidunt nunc pulvinar. Orci eu lobortis [...]</p>
+          <a href="" class="btn-leia_mais">leia mais</a>
+        </div>
+      </div>
+      <!-- FIM - ARTIGO 2 -->
+
+      <!-- INICIO - ARTIGO 3 -->
+      <div class="container mod-artigo">
+        <div class="img-artigo">
+          <a href=""><img src="img/categoria-1/cat-img-3.jpg" alt="Artigo 3" class="grid-5"></a>
+        </div>
+
+        <div class="text-artigo grid-11">
+          <h3 class="categoria-artigo">CATEGORIA</h3>
+          <a href="">
+            <h2 class="titulo-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor </h2>
+          </a>
+          <p class="autor-artigo">Publicado por [NOME] em [MES/ANO/]</p>
+          <p class="previa-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien eget mi proin sed. uis imperdiet massa tincidunt nunc pulvinar. Orci eu lobortis [...]</p>
+          <a href="" class="btn-leia_mais">leia mais</a>
+        </div>
+      </div>
+      <!-- FIM - ARTIGO 3 -->
+
+      <!-- INICIO - ARTIGO 4 -->
+      <div class="container mod-artigo">
+        <div class="img-artigo">
+          <a href=""><img src="img/categoria-1/cat-img-4.jpg" alt="Artigo 4" class="grid-5"></a>
+        </div>
+
+        <div class="text-artigo grid-11">
+          <h3 class="categoria-artigo">CATEGORIA</h3>
+          <a href="">
+            <h2 class="titulo-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor </h2>
+          </a>
+          <p class="autor-artigo">Publicado por [NOME] em [MES/ANO/]</p>
+          <p class="previa-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien eget mi proin sed. uis imperdiet massa tincidunt nunc pulvinar. Orci eu lobortis [...]</p>
+          <a href="" class="btn-leia_mais">leia mais</a>
+        </div>
+      </div>
+      <!-- FIM - ARTIGO 4 -->
+
+      <!-- INICIO - ARTIGO 5 -->
+      <div class="container mod-artigo">
+        <div class="img-artigo">
+          <a href=""><img src="img/categoria-1/cat-img-5.jpg" alt="Artigo 5" class="grid-5"></a>
+        </div>
+
+        <div class="text-artigo grid-11">
+          <h3 class="categoria-artigo">CATEGORIA</h3>
+          <a href="">
+            <h2 class="titulo-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor </h2>
+          </a>
+          <p class="autor-artigo">Publicado por [NOME] em [MES/ANO/]</p>
+          <p class="previa-artigo">Lorem ipsum dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien eget mi proin sed. uis imperdiet massa tincidunt nunc pulvinar. Orci eu lobortis [...]</p>
+          <a href="" class="btn-leia_mais">leia mais</a>
+        </div>
+      </div>
+
+      <nav class="container nav-cat-pgs">
+        <ul class="grid-16">
+          <li><a class="nav-cat-pgs nav-cat-pgs-ativo" href="page-embalagens-1.html">1</a></li>
+          <li><a href="page-embalagens-2.html">2</a></li>
+        </ul>
+      </nav>
+    </section>
+    <!-- FIM - ARTIGO 5 -->
+    
+    <!-- FIM - PRINCIPAIS ARTIGOS -->
+
+
+<?php endwhile; else: endif; ?>
+<?php get_footer(); ?>
