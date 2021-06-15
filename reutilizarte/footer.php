594272f8f66741d42c249e7f33f9836e013e8338
Leonardo Martins <sleonardo.mrt@gmail.com>
2021-06-15 19:02:53 -0300

    Footer do site

---
+++ reutilizarte/footer.php
@@ -0,0 +1,44 @@
+    <!-- INÍCIO - FOOTER -->
+    <footer>
+        <div class="footer">
+          <div class="container footer-container">
+            <div class="grid-6 footer-reutilizacao">
+              <h2>reutilização</h2>
+              <ul class="grid-3 lista-categorias">
+                <li><a href="/embalagens">Embalagens</a></li>
+                <li><a href="/garrafa-pet">Garrafa PET</a></li>
+                <li><a href="/papelao">Papelão</a></li>
+                
+              </ul>
+              <ul class="grid-2 lista-categorias">
+                <li><a href="/plastico">Plástico</a></li>
+                <li><a href="/outros">Outros</a></li>
+              </ul>
+            </div>
+          
+          
+            <div class="grid-7 footer-contato">
+              <h2>contato</h2>
+              <ul>
+                <li>projetoreutilizarte@gmail.com</li>
+                <li>(XX) XXXXX-XXXX</li>
+              </ul>
+            </div>
+
+            <div class="grid-3 footer-logo">
+              <img src="http://reutilizarte.rf.gd/wp-content/uploads/2021/06/Logo-2.png" alt="Reutilizarte" style="width: 175px;">
+            </div>
+
+          </div> 
+        </div> 
+        
+        <div class="copyright">
+          <div class="container">
+            <p class="grid-16">© Todos os direitos reservados - Grupo Dynamium</p>
+          </div>
+        </div>
+      <?php wp_footer(); ?>
+      </footer>
+    <!-- FIM - FOOTER -->
+    </body>
+</html>
